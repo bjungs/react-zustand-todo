a2b48ec573d4bf3fa9ecdb7e4a4b7631a97d6f6f
Bernardo J. C. Nunes <bernardo.nunes@inetum-realdolmen.world>
2022-03-29 12:13:26 +0200

    * added simple transition animation for added/removed items using React-Spring

--- src/components/todos/Todos.tsx
+++ src/components/todos/Todos.tsx
@@ -1,9 +1,26 @@
+import { animated, useTransition } from 'react-spring'
+
+import useTodoStore from '../../stores/todoStore'
+
 import Todo from './Todo'
 import { TodoForm } from './TodoForm'
-import useTodoStore from '../../stores/todoStore'
 
 export function Todos() {
   const { todos, addTodo, removeTodo, setTodoStatus } = useTodoStore()
+  const transitions = useTransition(todos, {
+    from: {
+      opacity: 0,
+      maxHeight: 0,
+    },
+    enter: {
+      opacity: 1,
+      maxHeight: 500,
+    },
+    leave: {
+      opacity: 0,
+      maxHeight: 0,
+    },
+  })
 
   return (
     <div className={`w-72 md:w-96`}>
@@ -11,13 +28,16 @@ export function Todos() {
       <TodoForm onNewTodo={addTodo} />
       <hr className={`my-6`} />
       <div>
-        {todos.map((todo, idx) => (
-          <Todo
-            key={idx}
-            {...todo}
-            onStatusChange={(completed) => setTodoStatus(idx, completed)}
-            onRemoveClick={() => removeTodo(idx)}
-          />
+        {transitions((styles, todo, _, idx) => (
+          <animated.div style={styles}>
+            <Todo
+              {...todo}
+              onStatusChange={(completed) => {
+                setTodoStatus(idx, completed)
+              }}
+              onRemoveClick={() => removeTodo(idx)}
+            />
+          </animated.div>
         ))}
       </div>
     </div>
